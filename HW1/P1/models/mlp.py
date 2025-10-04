120a27bc949bd2029a92986ed83983315959064c
brownepres <barnabas.epres@gmail.com>
2025-10-04 12:58:57 +0200

    finished with mytorch

--- HW1/P1/models/mlp.py
+++ HW1/P1/models/mlp.py
@@ -1,1 +1,49 @@
+import numpy as np
+
+from mytorch.nn.linear import Linear
+from mytorch.nn.activation import ReLU
+class MLP0:
+    def __init__(self):
+        self.layers = [Linear(2, 3), ReLU()]
+
+
+    def forward(self, A0):
+        Z0 = self.layers[0].forward(A0)
+        A1 = self.layers[1].forward(Z0)
+        return A1
+
     
+    def backward(self, dLdA1):
+       dLdZ0 = self.layers[1].backward(dLdA1)
+       dLdA0 = self.layers[0].backward(dLdZ0)
+       return dLdA0 
+    
+class MLP2:
+    def __init__(self):
+        self.layers = [Linear(2, 3), ReLU(), Linear(3, 2), ReLU()]
+
+    def forward(self, A0):
+        A1 = self.layers[1].forward(self.layers[0].forward(A0))
+        A2 = self.layers[3].forward(self.layers[2].forward(A1))
+        return A2
+
+    def backward(self, dLdA2):
+        dLdA1 = self.layers[2].backward(self.layers[3].backward(dLdA2))
+        dLdA0 = self.layers[0].backward(self.layers[1].backward(dLdA1))
+        return dLdA0
+    
+
+class MLP4:
+    def __init__(self):
+        self.layers = [Linear(2, 4), ReLU(), Linear(4, 8), ReLU(), Linear(8, 8), ReLU(), Linear(8, 4), ReLU(), Linear(4, 2), ReLU()]
+
+    def forward(self, A):
+        for i in range(len(self.layers)):
+            A = self.layers[i].forward(A)
+        return A
+    
+    def backward(self, dLdA):
+        for i in reversed(range(len(self.layers))):
+            dLdA = self.layers[i].backward(dLdA)
+
+        return dLdA
---
+++ HW1/P1/mytorch/nn/loss.py
@@ -0,0 +1,42 @@
+import numpy as np
+
+class MSELoss:
+    def forward(self, A, Y):
+        self.A, self.Y = A, Y
+        self.N, self.C = A.shape[0], A.shape[1]
+
+        se = (A-Y) * (A-Y)
+
+        col_ones = np.ones((self.C, 1))
+        row_ones = np.ones((self.N, 1))
+
+        sse = row_ones.T @ se @ col_ones
+        mse = sse / (self.N * self.C)
+
+        return mse
+    
+    def backward(self):
+        dLdA = 2 * (self.A - self.Y) / (self.N * self.C)
+
+        return dLdA
+    
+
+class CrossEntropyLoss:
+    def forward(self, A, Y):
+        self.A, self.Y = A, Y
+        self.N, self.C = A.shape[0], A.shape[1]
+
+        Ones_C = np.ones((self.C, 1))
+        Ones_N = np.ones((self.N, 1))        
+
+        self.softmax = np.exp(A) / np.sum(np.exp(A), axis=1).reshape(-1, 1)
+
+        crossentropy = (-Y * np.log(self.softmax)) @ Ones_C
+        sum_crossentropy = Ones_N.T @ crossentropy
+        L = sum_crossentropy / self.N
+        return L
+    
+    def backward(self):
+        dLdA = (self.softmax - self.Y) / self.N
+
+        return dLdA
